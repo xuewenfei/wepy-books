ad261d26aaf35cd87d63e326570513302fe5a8d6
steven.xue <steven.xue@wqxwf.local>
2018-01-02 14:57:25 +0800

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,10 @@ module.exports = {
   },
   // add your custom rules here
   'rules': {
+    'quotes': 0,
+    'eqeqeq': 0,
+    'no-unused-vars': 0,
+    'no-useless-escape': 0,
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
